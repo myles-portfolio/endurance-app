3a6c17d15ffd1cc45aaf08e045b42f858f5b1f53
mylesh <84793716+myles-portfolio@users.noreply.github.com>
2024-12-18 17:36:03 -0600

    setup and clerk config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,12 @@ import {
 	SignedOut,
 	UserButton,
 } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
 import "./globals.css";
+import { AppSidebar } from "./app-sidebar";
 
 export default function RootLayout({
 	children,
@@ -13,16 +18,28 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<ClerkProvider>
+		<ClerkProvider
+			appearance={{
+				baseTheme: dark,
+			}}
+		>
 			<html lang="en" className="dark">
 				<body>
-					<SignedOut>
-						<SignInButton />
-					</SignedOut>
-					<SignedIn>
-						<UserButton />
-					</SignedIn>
-					{children}
+					<header>
+						<SignedOut>
+							<SignInButton />
+						</SignedOut>
+						<SignedIn>
+							<UserButton />
+						</SignedIn>
+					</header>
+					<SidebarProvider>
+						<AppSidebar />
+						<main>
+							<SidebarTrigger />
+							{children}
+						</main>
+					</SidebarProvider>
 				</body>
 			</html>
 		</ClerkProvider>
